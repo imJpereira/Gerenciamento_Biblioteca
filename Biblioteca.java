97f4062fc1fdc9a26c98ba8a353522a666bd1b0a
Bruno <bruno.pf2005@gmail.com>
2024-09-28 18:59:06 -0300

    Alterações

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,26 @@ public class Biblioteca {
         for (Livro livro : acervo) {
             if (livro.getTitulo().equalsIgnoreCase(novoLivro.getTitulo())) {
                 throw new Exception("Já existe um livro com esse nome cadastrado");
+            }
+        }
+        if (novoLivro.getTitulo() == null || novoLivro.getAutor().isEmpty()){
+            throw new Exception("Não é permitido cadastrar livro sem autor!");
+        }
+        int anoAtual = LocalDate.now().getYear();
+        if (novoLivro.getAnoPublicacao() < 1400 || novoLivro.getAnoPublicacao() > anoAtual){
+            throw new Exception("Ano de publicação inválido. Deve estar entre 1400 e " + anoAtual + ".");
+        }
+        if  (novoLivro.getPaginas() <= 0){ 
+            throw new Exception("Número de páginas inválido. Deve ser maior que zero.");
+        
+        }
+        if (novoLivro.getAutor().toLowerCase().contains("autor")) {
+            throw new Exception("Não é permitido cadastrar o autor do livro como 'autor'!");
+        }
+        
+        for (Livro livro : acervo) {
+            if (livro.getTitulo().equalsIgnoreCase(novoLivro.getTitulo())) {
+                throw new Exception("Já existe um livro com esse nome cadastrado.");
             }
         }
         
